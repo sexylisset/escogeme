c962cebf29a28ac2fe53b99d211c329d01123563
sexylisset <sexylisset@gmail.com>
2016-10-20 13:26:23 -0400

    Add files via upload

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -9,25 +9,41 @@ app.controller('EscogeTabla',function($scope){
                 id:'lamer',
                 name:{esp:'Lamer', eng:"Lick"},
                 icon:'tongue.svg',
-                action:{url:'showImage',tipo:'ramdom',image:['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif']}
+                action:{
+                    url:'showImage',
+                    tipo:'ramdom',
+                    image:['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif']
+                }
             },
             {
                 id:'coger',
                 name:{esp:'Coger', eng:"Fuck"},
                 icon:'sex.svg',
-                action:{url:'showImage',tipo:'ramdom',image:['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif']}
+                action:{
+                    url:'showImage',
+                    tipo:'ramdom',
+                    image:['1.gif','2.gif','3.gif','4.gif','5.gif']
+                }
             },
             {
-                id:'mastubar',
+                id:'masturbar',
                 name:{esp:'Masturbar', eng:"Maturbate"},
                 icon:'masturbate.svg',
-                action:{url:'showImage',tipo:'ramdom',image:['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif']}
+                action:{
+                    url:'showImage',
+                    tipo:'ramdom',
+                    image:['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif','10.gif','11.gif','12.gif','13.gif','14.gif','15.gif']
+                }
             },
             {
                 id:'twerk',
                 name:'Twerk',
                 icon:'twerk.svg',
-                action:{url:'showImage',tipo:'ramdom',image:['1.gif','2.gif','3.gif','4.gif','5.gif','6.gif','7.gif','8.gif','9.gif']}
+                action:{
+                    url:'showImage',
+                    tipo:'ramdom',
+                    image:['1.gif','2.gif','3.gif','4.gif']
+                }
             }
         ]
     };
@@ -36,8 +52,3 @@ app.controller('EscogeTabla',function($scope){
         return x
     }
 });
-
-function ramdom(low, high) {
-    var x = Math.ceil(Math.random() * (high - low) + low);
-    return x
-}
